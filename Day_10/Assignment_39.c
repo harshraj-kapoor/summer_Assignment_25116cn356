3d33fff1b744680598e8a4c3dab48356c2a5e1fc
harsh raj <kapoorharshraj54@gmail.com>
2026-06-13 22:36:55 +0530

    Create Assignment_39.c

---
+++ Day_10/Assignment_39.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n, i, j, k;
+
+    printf("enter n");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (k = n - i; k >= 0; k--)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
