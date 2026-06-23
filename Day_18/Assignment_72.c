aad5d1ba7859ccdd2962d512dcff4b89e52eb8ff
Harsh <kapoorharshraj54@gmail.com>
2026-06-23 09:44:39 +0530

    Add assugnment72

--- Day_18/Assignment_72.c
+++ Day_18/Assignment_72.c
@@ -2,35 +2,31 @@
 
 int main()
 {
-    int n,j, i,temp;
-    printf("enter the number of elements : ");
-    scanf("%d", &n);
-    int a[n];
-    printf("enter the elements to be sorted : ");
-    for (i = 0; i < n; i++)
+  int n, j, i, temp;
+  printf("enter the number of elements : ");
+  scanf("%d", &n);
+  int a[n];
+  printf("enter the elements to be sorted : ");
+  for (i = 0; i < n; i++)
+  {
+    scanf("%d", &a[i]);
+  }
+  for (i = 0; i < n - 1; i++)
+  {
+    for (j = 0; j < n - 1 - i; j++)
     {
-        scanf("%d", &a[i]);
+      if (a[j] < a[j + 1])
+      {
+        temp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = temp;
+      }
     }
-    for ( i = 0; i < n-1; i++)
-    {
-       for ( j = 0; j < n-1-i; j++)
-       {
-        if (a[j]<a[j+1])   
-        {
-          temp=a[j];
-          a[j]=a[j+1];
-          a[j+1]=temp;
-        }
-        
-       }
-       
-    }
-    printf("the sorted array is : ");
-    for ( i = 0; i <n; i++)
-    {
-     printf("%d ",a[i]);
-     }
-     return 0;
-    }
-    
-    
+  }
+  printf("the sorted array is : ");
+  for (i = 0; i < n; i++)
+  {
+    printf("%d ", a[i]);
+  }
+  return 0;
+}
